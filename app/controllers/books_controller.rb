1cbe263578abb7c1eddd686c72384d129484c7df
aryaminus <sunim.54@gmail.com>
2017-01-19 14:48:09 +0545

    Form Setup

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,8 +3,16 @@ class BooksController < ApplicationController
 	end
 
 	def new
+		@book = Book.new
 	end
 
 	def create
+		@book = Book.new(book_params)
 	end
+
+	private
+		def book_params
+			params.require(:book).permit(:title, :description, :author)
+		end
 end
+
