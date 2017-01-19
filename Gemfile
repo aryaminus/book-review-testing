eb025efd192d0d078effe9c03b40d9a7a7cb8ee4
aryaminus <sunim.54@gmail.com>
2017-01-19 20:03:45 +0545

    Image handling and mapping

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'simple_form'
 gem 'bootstrap-sass', '~> 3.3.6'
 gem 'devise', '~> 4.2'
 gem 'rails_bootstrap_navbar', '~> 2.0', '>= 2.0.1'
+gem 'paperclip', '~> 5.1'
 
 
 # Use jquery as the JavaScript library
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ActiveRecord::Base
     belongs_to :user
      belongs_to :category
+       has_attached_file :book_img, styles => { :book_index => "200x300>", :book_show => "300x400>" }, default_url: "/images/:style/missing.png"
+        validates_attachment_content_type :book_img, content_type: /\Aimage\/.*\z/
 end
